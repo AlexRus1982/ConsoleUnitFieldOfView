4dc58fe03ad5c67ae775f86fa009e4b0b18c3e34
Rusakov Alex <alexanderwk@yandex.ru>
2022-01-10 21:51:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleUnitFieldOfView/ConsoleUnitFieldOfView.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+class TaskInfo {
+
+public:
+	TaskInfo ();
+	~TaskInfo ();
+
+	int dimX;			// ширина 2-х мерного поля с юнитами
+	int dimY;			// высота 2-х мерного поля с юнитами
+
+};
+
+TaskInfo ::TaskInfo () {
+}
+
+TaskInfo ::~TaskInfo () {
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
